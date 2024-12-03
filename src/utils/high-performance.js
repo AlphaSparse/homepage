d0def0b7fbd0df1c789442ccc5a4a96603738008
WangYS <wys2013@aliyun.com>
2024-12-03 12:08:41 +0800

    Update high-performance.js

--- src/utils/high-performance.js
+++ src/utils/high-performance.js
@@ -12,21 +12,21 @@ export default [
                     label: 'single',
                     file: [
                       {
-                        url: '/AlphaSparseLib/Alphasparse/Hygon-DCU/spmv/csr/spmv_csr_s_dist_dcu.png',
+                        url: '/AlphaSparseLib/Alphasparse/Hygon-DCU/spmv/csr/single/spmv_csr_s_dist_dcu.png',
                         name: 'spmv_csr_s_dist_dcu.png',
                       },
                       {
-                        url: '/AlphaSparseLib/Alphasparse/Hygon-DCU/spmv/csr/spmv_csr_s_pdf_dcu.png',
+                        url: '/AlphaSparseLib/Alphasparse/Hygon-DCU/spmv/csr/single/spmv_csr_s_pdf_dcu.png',
                         name: 'spmv_csr_s_pdf_dcu.png',
                       },
                       {
-                        url: '/AlphaSparseLib/Alphasparse/Hygon-DCU/spmv/csr/spmv_csr_s_bestAlg_dcu.png',
+                        url: '/AlphaSparseLib/Alphasparse/Hygon-DCU/spmv/csr/single/spmv_csr_s_bestAlg_dcu.png',
                         name: 'spmv_csr_s_bestAlg_dcu.png',
                       },                      
                     ],
                     dataSource: [
                       {
-                        url: '/AlphaSparseLib/Alphasparse/Hygon-DCU/spmv/csr/spmv_csr_s_dcu.csv',
+                        url: '/AlphaSparseLib/Alphasparse/Hygon-DCU/spmv/csr/single/spmv_csr_s_dcu.csv',
                         name: 'spmv_csr_s_dcu.csv',
                       },
                     ],
@@ -36,21 +36,21 @@ export default [
                     label: 'double',
                     file: [
                       {
-                        url: '/AlphaSparseLib/Alphasparse/Hygon-DCU/spmv/csr/spmv_csr_d_dist_dcu.png',
+                        url: '/AlphaSparseLib/Alphasparse/Hygon-DCU/spmv/csr/double/spmv_csr_d_dist_dcu.png',
                         name: 'spmv_csr_d_dist_dcu.png',
                       },
                       {
-                        url: '/AlphaSparseLib/Alphasparse/Hygon-DCU/spmv/csr/spmv_csr_d_pdf_dcu.png',
+                        url: '/AlphaSparseLib/Alphasparse/Hygon-DCU/spmv/csr/double/spmv_csr_d_pdf_dcu.png',
                         name: 'spmv_csr_d_pdf_dcu.png',
                       },
                       {
-                        url: '/AlphaSparseLib/Alphasparse/Hygon-DCU/spmv/csr/spmv_csr_d_bestAlg_dcu.png',
+                        url: '/AlphaSparseLib/Alphasparse/Hygon-DCU/spmv/csr/double/spmv_csr_d_bestAlg_dcu.png',
                         name: 'spmv_csr_d_bestAlg_dcu.png',
                       },                      
                     ],
                     dataSource: [
                       {
-                        url: '/AlphaSparseLib/Alphasparse/Hygon-DCU/spmv/csr/spmv_csr_d_dcu.csv',
+                        url: '/AlphaSparseLib/Alphasparse/Hygon-DCU/spmv/csr/double/spmv_csr_d_dcu.csv',
                         name: 'spmv_csr_d_dcu.csv',
                       },
                     ],
@@ -84,21 +84,21 @@ export default [
                     label: 'single',
                     file: [
                       {
-                        url: '/AlphaSparseLib/Alphasparse/NVIDIA-GPU/spmv/csr/spmv_csr_s_dist_gpu.png',
+                        url: '/AlphaSparseLib/Alphasparse/NVIDIA-GPU/spmv/csr/single/spmv_csr_s_dist_gpu.png',
                         name: 'spmv_csr_s_dist_gpu.png',
                       },
                       {
-                        url: '/AlphaSparseLib/Alphasparse/NVIDIA-GPU/spmv/csr/spmv_csr_s_pdf_gpu.png',
+                        url: '/AlphaSparseLib/Alphasparse/NVIDIA-GPU/spmv/csr/single/spmv_csr_s_pdf_gpu.png',
                         name: 'spmv_csr_s_pdf_gpu.png',
                       },
                       {
-                        url: '/AlphaSparseLib/Alphasparse/NVIDIA-GPU/spmv/csr/spmv_csr_s_bestAlg_gpu.png',
+                        url: '/AlphaSparseLib/Alphasparse/NVIDIA-GPU/spmv/csr/single/spmv_csr_s_bestAlg_gpu.png',
                         name: 'spmv_csr_s_bestAlg_gpu.png',
                       },                      
                     ],
                     dataSource: [
                       {
-                        url: '/AlphaSparseLib/Alphasparse/NVIDIA-GPU/spmv/csr/spmv_csr_s_gpu.csv',
+                        url: '/AlphaSparseLib/Alphasparse/NVIDIA-GPU/spmv/csr/single/spmv_csr_s_gpu.csv',
                         name: 'spmv_csr_s_gpu.csv',
                       },
                     ],
@@ -108,21 +108,21 @@ export default [
                     label: 'double',
                     file: [
                       {
-                        url: '/AlphaSparseLib/Alphasparse/NVIDIA-GPU/spmv/csr/spmv_csr_d_dist_gpu.png',
+                        url: '/AlphaSparseLib/Alphasparse/NVIDIA-GPU/spmv/csr/double/spmv_csr_d_dist_gpu.png',
                         name: 'spmv_csr_d_dist_gpu.png',
                       },
                       {
-                        url: '/AlphaSparseLib/Alphasparse/NVIDIA-GPU/spmv/csr/spmv_csr_d_pdf_gpu.png',
+                        url: '/AlphaSparseLib/Alphasparse/NVIDIA-GPU/spmv/csr/double/spmv_csr_d_pdf_gpu.png',
                         name: 'spmv_csr_d_pdf_gpu.png',
                       },
                       {
-                        url: '/AlphaSparseLib/Alphasparse/NVIDIA-GPU/spmv/csr/spmv_csr_d_bestAlg_gpu.png',
+                        url: '/AlphaSparseLib/Alphasparse/NVIDIA-GPU/spmv/csr/double/spmv_csr_d_bestAlg_gpu.png',
                         name: 'spmv_csr_d_bestAlg_gpu.png',
                       },                      
                     ],
                     dataSource: [
                       {
-                        url: '/AlphaSparseLib/Alphasparse/NVIDIA-GPU/spmv/csr/spmv_csr_d_gpu.csv',
+                        url: '/AlphaSparseLib/Alphasparse/NVIDIA-GPU/spmv/csr/double/spmv_csr_d_gpu.csv',
                         name: 'spmv_csr_d_gpu.csv',
                       },
                     ],
