09ea4e5224b3e83b6911895f7017f6da1ea4aaa4
WangYS <wys2013@aliyun.com>
2024-12-08 19:17:48 +0800

    Update high-performance.js

--- src/utils/high-performance.js
+++ src/utils/high-performance.js
@@ -3,6 +3,76 @@ export default [
     label: 'AlphaSparseLib',
     children: [
       {
+        label: 'Hygon-CPU',
+        children: [
+          {
+            label: 'spmv',
+            children: [
+              {
+                label: 'single_1thread',
+                file: [
+                  {
+                    url: '/Alphasparse/Hygon-CPU/spmv/csr/single/spmv_csr_s_dist_cpu.png',
+                    name: 'spmv_csr_s_dist_cpu.png',
+                  },
+                  {
+                    url: '/Alphasparse/Hygon-CPU/spmv/csr/single/spmv_csr_s_pdf_cpu.png',
+                    name: 'spmv_csr_s_pdf_cpu.png',
+                  },
+                  {
+                    url: '/Alphasparse/Hygon-CPU/spmv/csr/single/spmv_csr_s_bestAlg_cpu.png',
+                    name: 'spmv_csr_s_bestAlg_cpu.png',
+                  },
+                ],
+                dataSource: [
+                  {
+                    url: '/Alphasparse/Hygon-CPU/spmv/csr/single/spmv_csr_s_alg_cpu.csv',
+                    name: 'spmv_csr_s_alg_cpu.csv',
+                  },
+                ],
+                textInfo: ['测试信息', '测试平台: 海光CPU Z100'],
+              },
+              {
+                label: 'single_1numa_8thread',
+                file: [
+                  {
+                    url: '/Alphasparse/Hygon-CPU/spmv/csr/single/spmv_csr_s_dist_cpu_numa.png',
+                    name: 'spmv_csr_s_dist_cpu_numa.png',
+                  },
+                  {
+                    url: '/Alphasparse/Hygon-CPU/spmv/csr/single/spmv_csr_s_pdf_cpu_numa.png',
+                    name: 'spmv_csr_s_pdf_cpu_numa.png',
+                  },
+                  {
+                    url: '/Alphasparse/Hygon-CPU/spmv/csr/single/spmv_csr_s_bestAlg_cpu_numa.png',
+                    name: 'spmv_csr_s_bestAlg_cpu_numa.png',
+                  },
+                ],
+                dataSource: [
+                  {
+                    url: '/Alphasparse/Hygon-CPU/spmv/csr/single/spmv_csr_s_alg_cpu_numa.csv',
+                    name: 'spmv_csr_s_alg_cpu_numa.csv',
+                  },
+                ],
+                textInfo: ['测试信息', '测试平台: 海光DCU Z100'],
+              },
+            ],
+          },
+          {
+            label: 'spmm',
+          },
+          {
+            label: 'spgemm',
+          },
+          {
+            label: 'sptrsv',
+          },
+          {
+            label: 'sptrsm',
+          },
+        ],
+      },
+      {
         label: 'Hygon-DCU',
         children: [
           {
@@ -12,21 +82,21 @@ export default [
                 label: 'single',
                 file: [
                   {
-                    url: '/AlphaSparseLib/Alphasparse/Hygon-DCU/spmv/csr/single/spmv_csr_s_dist_dcu.png',
+                    url: '/Alphasparse/Hygon-DCU/spmv/csr/single/spmv_csr_s_dist_dcu.png',
                     name: 'spmv_csr_s_dist_dcu.png',
                   },
                   {
-                    url: '/AlphaSparseLib/Alphasparse/Hygon-DCU/spmv/csr/single/spmv_csr_s_pdf_dcu.png',
+                    url: '/Alphasparse/Hygon-DCU/spmv/csr/single/spmv_csr_s_pdf_dcu.png',
                     name: 'spmv_csr_s_pdf_dcu.png',
                   },
                   {
-                    url: '/AlphaSparseLib/Alphasparse/Hygon-DCU/spmv/csr/single/spmv_csr_s_bestAlg_dcu.png',
+                    url: '/Alphasparse/Hygon-DCU/spmv/csr/single/spmv_csr_s_bestAlg_dcu.png',
                     name: 'spmv_csr_s_bestAlg_dcu.png',
                   },
                 ],
                 dataSource: [
                   {
-                    url: '/AlphaSparseLib/Alphasparse/Hygon-DCU/spmv/csr/single/spmv_csr_s_dcu.csv',
+                    url: '/Alphasparse/Hygon-DCU/spmv/csr/single/spmv_csr_s_dcu.csv',
                     name: 'spmv_csr_s_dcu.csv',
                   },
                 ],
@@ -36,21 +106,21 @@ export default [
                 label: 'double',
                 file: [
                   {
-                    url: '/AlphaSparseLib/Alphasparse/Hygon-DCU/spmv/csr/double/spmv_csr_d_dist_dcu.png',
+                    url: '/Alphasparse/Hygon-DCU/spmv/csr/double/spmv_csr_d_dist_dcu.png',
                     name: 'spmv_csr_d_dist_dcu.png',
                   },
                   {
-                    url: '/AlphaSparseLib/Alphasparse/Hygon-DCU/spmv/csr/double/spmv_csr_d_pdf_dcu.png',
+                    url: '/Alphasparse/Hygon-DCU/spmv/csr/double/spmv_csr_d_pdf_dcu.png',
                     name: 'spmv_csr_d_pdf_dcu.png',
                   },
                   {
-                    url: '/AlphaSparseLib/Alphasparse/Hygon-DCU/spmv/csr/double/spmv_csr_d_bestAlg_dcu.png',
+                    url: '/Alphasparse/Hygon-DCU/spmv/csr/double/spmv_csr_d_bestAlg_dcu.png',
                     name: 'spmv_csr_d_bestAlg_dcu.png',
                   },
                 ],
                 dataSource: [
                   {
-                    url: '/AlphaSparseLib/Alphasparse/Hygon-DCU/spmv/csr/double/spmv_csr_d_dcu.csv',
+                    url: '/Alphasparse/Hygon-DCU/spmv/csr/double/spmv_csr_d_dcu.csv',
                     name: 'spmv_csr_d_dcu.csv',
                   },
                 ],
@@ -60,6 +130,56 @@ export default [
           },
           {
             label: 'spmm',
+             children: [
+              {
+                label: 'single',
+                file: [
+                  {
+                    url: '/Alphasparse/Hygon-DCU/spmm/csr/single/spmm_csr_rm_c64_s_dist_dcu.png',
+                    name: 'spmm_csr_rm_c64_s_dist_dcu.png',
+                  },
+                  {
+                    url: '/Alphasparse/Hygon-DCU/spmm/csr/single/spmm_csr_rm_c64_s_pdf_dcu.png',
+                    name: 'spmm_csr_rm_c64_s_pdf_dcu.png',
+                  },
+                  {
+                    url: '/Alphasparse/Hygon-DCU/spmm/csr/single/spmm_csr_rm_c64_s_bestAlg_dcu.png',
+                    name: 'spmm_csr_rm_c64_s_bestAlg_dcu.png',
+                  },
+                ],
+                dataSource: [
+                  {
+                    url: '/Alphasparse/Hygon-DCU/spmm/csr/single/f32_64_row.csv',
+                    name: 'spmm_csr_rm_c64_f32.csv',
+                  },
+                ],
+                textInfo: ['测试信息', '测试平台: 海光DCU Z100'],
+              },
+              {
+                label: 'double',
+                file: [
+                  {
+                    url: '/Alphasparse/Hygon-DCU/spmm/csr/double/spmm_csr_rm_c64_d_dist_dcu.png',
+                    name: 'spmm_csr_rm_c64_d_dist_dcu.png',
+                  },
+                  {
+                    url: '/Alphasparse/Hygon-DCU/spmm/csr/double/spmm_csr_rm_c64_d_pdf_dcu.png',
+                    name: 'spmm_csr_rm_c64_d_pdf_dcu.png',
+                  },
+                  {
+                    url: '/Alphasparse/Hygon-DCU/spmm/csr/double/spmm_csr_rm_c64_d_bestAlg_dcu.png',
+                    name: 'spmm_csr_rm_c64_d_bestAlg_dcu.png',
+                  },
+                ],
+                dataSource: [
+                  {
+                    url: '/Alphasparse/Hygon-DCU/spmm/csr/double/f64_64_row.csv',
+                    name: 'spmm_csr_rm_c64_f64.csv',
+                  },
+                ],
+                textInfo: ['测试信息', '测试平台: 海光DCU Z100'],
+              },
+            ],
           },
           {
             label: 'spgemm',
@@ -129,19 +249,69 @@ export default [
             ],
           },
           {
-            label: 'spmm',
+            label: 'spmm',        
           },
           {
             label: 'spgemm',
           },
           {
             label: 'sptrsv',
+               children: [
+              {
+                label: 'single',
+                file: [
+                  {
+                    url: '/Alphasparse/NVIDIA-GPU/trsv/csr/single/sptrsv_csr_w-pre_analysis_s_dist_gpu.png',
+                    name: 'sptrsv_csr_w-pre_analysis_s_dist_gpu.png',
+                  },
+                  {
+                    url: '/Alphasparse/NVIDIA-GPU/trsv/csr/single/sptrsv_csr_w-pre_solve_s_dist_gpu.png',
+                    name: 'sptrsv_csr_w-pre_solve_s_dist_gpu.png',
+                  },
+                  {
+                    url: '/Alphasparse/NVIDIA-GPU/trsv/csr/single/sptrsv_csr_w-pre_s_break-even_point_gpu.png',
+                    name: 'sptrsv_csr_w-pre_s_break-even_point_gpu.png',
+                  },
+                ],
+                dataSource: [
+                  {
+                    url: '/Alphasparse/NVIDIA-GPU/trsv/csr/sptrsv_gpu.csv',
+                    name: 'sptrsv_gpu.csv',
+                  },
+                ],
+                textInfo: ['测试信息', '测试平台: NVIDIA-GPU A100'],
+              },
+              {
+                label: 'double',
+                file: [
+                  {
+                    url: '/Alphasparse/NVIDIA-GPU/trsv/csr/double/sptrsv_csr_w-pre_analysis_d_dist_gpu.png',
+                    name: 'sptrsv_csr_w-pre_analysis_d_dist_gpu.png',
+                  },
+                  {
+                    url: '/Alphasparse/NVIDIA-GPU/trsv/csr/double/sptrsv_csr_w-pre_solve_d_dist_gpu.png',
+                    name: 'sptrsv_csr_w-pre_solve_d_dist_gpu.png',
+                  },
+                  {
+                    url: '/Alphasparse/NVIDIA-GPU/trsv/csr/double/sptrsv_csr_w-pre_d_break-even_point_gpu.png',
+                    name: 'sptrsv_csr_w-pre_d_break-even_point_gpu.png',
+                  },
+                ],
+                dataSource: [
+                  {
+                    url: '/Alphasparse/NVIDIA-GPU/trsv/csr/sptrsv_gpu.csv',
+                    name: 'sptrsv_gpu.csv',
+                  },
+                ],
+                textInfo: ['测试信息', '测试平台: NVIDIA-GPU A100'],
+              },
+            ],         
           },
           {
             label: 'sptrsm',
           },
         ],
-      },
+      },      
     ],
   },
 ];
